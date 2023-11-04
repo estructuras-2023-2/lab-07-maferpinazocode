a5aa3060fdd88283be9f3d40d4c98de7ffdc2ee7
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2023-11-04 02:38:33 +0000

    Se inicializan algunas variables para llevar el control de las victorias, el jugador actual y el retador, y un vector para almacenar el resultado del juego

--- main.cpp
+++ main.cpp
@@ -7,12 +7,18 @@ vector<int> Torneo(vector<int> habilidades, int N, int K) {
     int size = habilidades.size();
     int ganador = 0;
     int perdedor = 0;
-    
+
     queue<int> cola;
 
     for (int i = 2; i < tam; ++i) {
     cola.push(habilidades[i]);
-}
+    }
+
+    int victorias = 0;
+    int jugadorActual = habilidades[0];
+    int retador = habilidades[1];
+    vector<int> resultado(2);
+
 }
 
 int main(){
