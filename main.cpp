d51b15980db0e175260d76870c7664364c2ce020
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2023-11-04 02:28:02 +0000

    Declarando las variables para el tamaño (size), el ganador y el perdedor, de la misma manera, una cola para almacenar las habilidades de los jugadores

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 
 vector<int> Torneo(vector<int> habilidades, int N, int K) {
-
+    int tam = habilidades.size();
+    int ganador = 0;
+    int perdedor = 0;
+    queue<int> cola;
 }
 
 int main(){
-    
+
 }
